3e90029f3439ea684f2f0d5c445c43a89e46121e
Chelsea <chelseahannan@icloud.com>
2020-05-03 00:44:53 -0400

    add test files

--- pageObjects/checkpoint2Objects.js
+++ pageObjects/checkpoint2Objects.js
@@ -1,6 +1,58 @@
 module.exports = {
     url: 'https://devmountain-qa.github.io/Automation-Basics/build/',
     elements: {
-        
-    }
+
+        eOInput: {
+            selector: 'input[name="evenOddInput"]'
+        },
+        eOBtn: {
+            selector: 'button[name="evenOddButton"]'
+        },
+        evenRes: {
+            selector: 'span[name="evenResults"]'
+        },
+        oddRes: {
+            selector: 'span[name="oddResults"]'
+        },
+        fOInput: {
+            selector: 'input[name="objectFilterInput"]'
+        },
+        fOBtn: {
+            selector: 'button[name="objectFilterButton"]'
+        },
+        fORes: {
+            selector: 'span[name="objectFilterResults"]'
+        },
+        fSInput: {
+            selector: 'input[id="nameFilterInput"]'
+        },
+        fSBtn: {
+            selector: 'button[id="nameFilterButton"]'
+        },
+        fSRes: {
+            selector: 'span[name="nameFilterResults"]'
+        },
+        palInput: {
+            selector: 'input[name="palindromeInput"]'
+        },
+        palBtn: {
+            selector: 'button[name="palindromeButton"]'
+        },
+        palRes: {
+            selector: 'span[name="palindromeResults"]'
+        },
+        sumInput1: {
+            selector: 'input[name="sumInput1"]'
+        },
+        sumInput2: {
+            selector: 'input[name="sumInput2"]'
+        },
+        sumBtn: {
+            selector: 'button[name="sumButton"]'
+        },
+        sumRes: {
+            selector: 'span[name="sumResults"]'
+        }
+    },
+
 }
--- tests/checkpoint2Tests.js
+++ tests/checkpoint2Tests.js
@@ -1,4 +1,12 @@
-var checkpoint2 {}
+var checkpoint2
+var num = '12'
+var filterObj = 'name'
+var filterName = 'Jimmy Joe'
+var filterString = 'Maddy'
+var pal = 'madam'
+var num1 = 1
+var num2 = 2
+var num3 = num1 + num2
 
 module.exports = {
     beforeEach: browser => {
@@ -6,17 +14,51 @@ module.exports = {
         checkpoint2.navigate()
     },
 
-    // Tests here
-    'test': browser => {
-        checkpoint2
-            .click()
-            
+    after: browser => {
+        checkpoint2.end()
     },
 
-    'test': browser => {
+    // Tests here
+    'Evens and Odds': browser => {
         checkpoint2
-            .click()
-            
+            .setValue('@eOInput', num)
+        checkpoint2
+            .click('@eOBtn')
+            .verify.containsText('@evenRes', num2)
+            .verify.containsText('@oddRes', num1)     
+    },
+
+    'Filter Object': browser => {
+        checkpoint2
+            .setValue('@fOInput', filterObj)
+        checkpoint2
+            .click('@fOBtn')
+            .verify.containsText('@fORes', filterName);        
+    },
+
+    'Filter String': browser => {
+        checkpoint2
+            .setValue('@fSInput', filterString)
+        checkpoint2
+            .click('@fSBtn')
+            .verify.containsText('@fSRes', filterString)   
+    },
+
+    'Palindrome': browser => {
+        checkpoint2
+            .setValue('@palInput', pal)
+        checkpoint2
+            .click('@palBtn')
+            .verify.containsText('@palRes', 'true')
+    },
+
+    'Sum': browser => {
+        checkpoint2
+            .setValue('@sumInput1', num1)
+            .setValue('@sumInput2', num2)
+        checkpoint2
+            .click('@sumBtn')
+            .verify.containsText('@sumRes', num3)
     },
 
 
