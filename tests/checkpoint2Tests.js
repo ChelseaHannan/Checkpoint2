9c278153e10cb06677bf0e4d2ca2bace41de6ac8
Chelsea <chelseahannan@icloud.com>
2020-05-03 17:50:55 -0400

    add more tests

--- tests/checkpoint2Tests.js
+++ tests/checkpoint2Tests.js
@@ -2,11 +2,19 @@ var checkpoint2
 var num = '12'
 var filterObj = 'name'
 var filterName = 'Jimmy Joe'
+var filterObj1 = 'hairColor'
+var filterHair = 'brown'
 var filterString = 'Maddy'
+var filterStrings = 'James Jessica'
+var filterStrings1 = 'James, Jessica'
 var pal = 'madam'
+var notPal = 'moon'
 var num1 = 1
 var num2 = 2
 var num3 = num1 + num2
+var dec1 = 1.5
+var dec2 = 2.3
+var dec3 = 3.8
 
 module.exports = {
     beforeEach: browser => {
@@ -18,7 +26,7 @@ module.exports = {
         checkpoint2.end()
     },
 
-    // Tests here
+    Tests here
     'Evens and Odds': browser => {
         checkpoint2
             .setValue('@eOInput', num)
@@ -28,12 +36,29 @@ module.exports = {
             .verify.containsText('@oddRes', num1)     
     },
 
-    'Filter Object': browser => {
+    'Evens and Odds null entry': browser => {
+        checkpoint2
+            .setValue('@eOInput', filterString)
+        checkpoint2
+            .click('@eOBtn')
+            .verify.containsText('@evenRes', 'null')
+            .verify.containsText('@oddRes', 'null')     
+    },
+
+    'Filter Object search by name': browser => {
         checkpoint2
             .setValue('@fOInput', filterObj)
         checkpoint2
             .click('@fOBtn')
             .verify.containsText('@fORes', filterName);        
+    },
+
+    'Filter Object search by hair color': browser => {
+        checkpoint2
+            .setValue('@fOInput', filterObj1)
+        checkpoint2
+            .click('@fOBtn')
+            .verify.containsText('@fORes', filterHair);        
     },
 
     'Filter String': browser => {
@@ -44,12 +69,44 @@ module.exports = {
             .verify.containsText('@fSRes', filterString)   
     },
 
+    'Filter String use invalid name': browser => {
+        checkpoint2
+            .setValue('@fSInput', 'filterName')
+        checkpoint2
+            .click('@fSBtn')
+            .verify.containsText('@fSRes', '')   
+    },
+
+    'Filter String input multiple strings space separated': browser => {
+        checkpoint2
+            .setValue('@fSInput', filterStrings)
+        checkpoint2
+            .click('@fSBtn')
+            .verify.containsText('@fSRes', filterStrings)   
+    },
+
+    'Filter String input multiple strings comma separated': browser => {
+        checkpoint2
+            .setValue('@fSInput', filterStrings1)
+        checkpoint2
+            .click('@fSBtn')
+            .verify.containsText('@fSRes', filterStrings1)   
+    },
+
     'Palindrome': browser => {
         checkpoint2
             .setValue('@palInput', pal)
         checkpoint2
             .click('@palBtn')
             .verify.containsText('@palRes', 'true')
+    },
+
+    'Not a palindrome': browser => {
+        checkpoint2
+            .setValue('@palInput', notPal)
+        checkpoint2
+            .click('@palBtn')
+            .verify.containsText('@palRes', 'false')
     },
 
     'Sum': browser => {
@@ -60,6 +117,24 @@ module.exports = {
             .click('@sumBtn')
             .verify.containsText('@sumRes', num3)
     },
+
+    'Sum leave one blank': browser => {
+        checkpoint2
+            .setValue('@sumInput1', num1)
+            .setValue('@sumInput2', '')
+        checkpoint2
+            .click('@sumBtn')
+            .verify.containsText('@sumRes', num1)
+    },
+
+    'Sum with decimals': browser => {
+        checkpoint2
+            .setValue('@sumInput1', dec1)
+            .setValue('@sumInput2', dec2)
+        checkpoint2
+            .click('@sumBtn')
+            .verify.containsText('@sumRes', dec3)
+    }
 
 
 }
